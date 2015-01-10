a48c78fde378e5e90cd00e9c9b78079d0c903ee2
s.degambe@gmail.com <Sebad#21>
2015-01-10 17:55:56 +0100

    app

---
+++ App1/App1.Shared/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Popups;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace App1.ViewModel
+{
+	public class MainViewModel : ViewModelBase
+	{
+		public MainViewModel()
+		{
+			LabelDescription = "ClickMe";
+		}
+
+		private string _labelDescription;
+
+		public string LabelDescription
+		{
+			get { return _labelDescription; }
+			set
+			{
+				_labelDescription = value;
+				RaisePropertyChanged(() => LabelDescription);
+			}
+		}
+
+		private RelayCommand<string> _firstCommand;
+
+		public RelayCommand<string> FirstCommand
+		{
+			get
+			{
+				return _firstCommand ?? (_firstCommand = new RelayCommand<string>(async e =>
+										 {
+											 var dialog = new MessageDialog(e);
+											 await dialog.ShowAsync();
+										 }
+					));
+			}
+		}
+	}
+
+}
